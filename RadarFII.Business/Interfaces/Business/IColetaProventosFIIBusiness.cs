4c7e47df41ae44739adbab4fa33c58a3b3f14a71
VitorSancho <vitor.sancho07@gmail.com>
2023-06-26 20:32:40 -0300

    Ajuste na interface de business

---
+++ RadarFII.Business/Interfaces/Business/IColetaProventosFIIBusiness.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadarFII.Business.Interfaces.Business
+{
+    public interface IColetaProventosFIIBusiness
+    {
+        Task Coleta();
+    }
+}
